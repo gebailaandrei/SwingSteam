d064a12912be7574409c939b6e016009e07b8772
Gebi <andreigebaila@gmail.com>
2022-10-07 14:39:19 +0100

    Finished the bottom panel

--- src/MainFramePanels/BottomPanel.java
+++ src/MainFramePanels/BottomPanel.java
@@ -2,15 +2,82 @@ package MainFramePanels;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class BottomPanel extends JPanel {
+public class BottomPanel extends JPanel implements MouseListener {
 
-
+    JButton friendsAndChatBtn = new JButton(), addAGameBtn = new JButton(), downloadsBtn = new JButton();
+    JPanel leftPanel = new JPanel(), midPanel = new JPanel(), rightPanel = new JPanel();
+    Icon chatIcon = new ImageIcon("src/MainFramePanels/BottomPanelComponents/FriendsAndChatIcon.png");
+    Icon addGameIcon = new ImageIcon("src/MainFramePanels/BottomPanelComponents/AddAGameIcon.png");
 
     public BottomPanel(){
 
-        this.setBackground(new Color(46, 47, 48));
+        this.setBackground(new Color(42, 46, 51));
+        this.setLayout(new GridLayout(1, 3));
+
+        for (JPanel panel : new JPanel[]{leftPanel, midPanel, rightPanel}){
+            panel.setOpaque(false);
+            panel.setLayout(new BorderLayout());
+        }
+
+        for(JButton button : new JButton[]{friendsAndChatBtn, downloadsBtn, addAGameBtn}){
+            button.setFocusable(false);
+            button.setOpaque(false);
+            button.setContentAreaFilled(false);
+            button.setBorderPainted(false);
+            button.setForeground(new Color(173, 173, 174));
+            button.addMouseListener(this);
+            button.setPreferredSize(new Dimension(150, 30));
+
+            if(button.equals(friendsAndChatBtn)){
+                friendsAndChatBtn.setIcon(chatIcon);
+                friendsAndChatBtn.setText("Chat");
+                friendsAndChatBtn.setHorizontalTextPosition(SwingConstants.LEFT);
+            }else if(button.equals(downloadsBtn)){
+                downloadsBtn.setText("Downloads");
+            }else{
+                addAGameBtn.setIcon(addGameIcon);
+                addAGameBtn.setText("Add a game");
+                addAGameBtn.setMargin(new Insets(0, 25, 0, 0));
+            }
+        }
+
+        leftPanel.add(addAGameBtn, BorderLayout.WEST);
+        midPanel.add(downloadsBtn, BorderLayout.CENTER);
+        rightPanel.add(friendsAndChatBtn, BorderLayout.EAST);
+
+        this.add(leftPanel);
+        this.add(midPanel);
+        this.add(rightPanel);
 
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        JButton btn = (JButton)e.getSource();
+        btn.setForeground(Color.GRAY);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        JButton btn = (JButton)e.getSource();
+        btn.setForeground(new Color(173, 173, 174));
+    }
 }
