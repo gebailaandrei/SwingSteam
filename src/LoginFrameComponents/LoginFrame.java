7e36df85b107b6b19a6f4bb23865e3d894a80168
Gebi <andreigebaila@gmail.com>
2022-10-07 22:33:47 +0100

    Added Sign in box and remember me chack box

--- src/LoginFrameComponents/LoginFrame.java
+++ src/LoginFrameComponents/LoginFrame.java
@@ -6,54 +6,94 @@ import java.awt.*;
 
 public class LoginFrame extends JFrame{
 
-    Icon steamIcon = new ImageIcon("src/LoginFrameComponents/SteamIcon.png");
+    ImageIcon steamIcon = new ImageIcon("src/LoginFrameComponents/SteamIcon.png");
+    ImageIcon signInIcon = new ImageIcon("src/LoginFrameComponents/SignInIcon.png");
+
+
     JLabel accNameLbl, passLbl, helpLbl, newUserLbl;
     JTextField userTxt, passTxt;
-    JButton loginBtn, cancelBtn, helpBtn, newAccBtn;
-    JRadioButton rememberPass;
+    JButton loginBtn, helpBtn, newAccBtn;
+    JCheckBox rememberPass;
     JPanel panel = new JPanel();
 
     public LoginFrame(){
         this.setTitle("Steam Sign In");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setMinimumSize(new Dimension(500, 350));
+        this.setMinimumSize(new Dimension(700, 500));
         this.setResizable(false);
 
-        panel.setBackground(new Color(42, 46, 51));
+        panel.setBackground(new Color(33, 35, 40));
         panel.setLayout(null);
 
-        JLabel steamLabel = new JLabel();
-        steamLabel.setIcon(steamIcon);
-        steamLabel.setBounds(70, 10, 120, 60);
-        panel.add(steamLabel);
+        setFields();
+        setCheckBoxAndSingIn();
 
-        accNameLbl = new JLabel("Username:");
-        accNameLbl.setForeground(new Color(173, 173, 174));
-        accNameLbl.setBounds(70, 75, 70, 10);
-        panel.add(accNameLbl);
 
-        userTxt = new JTextField();
-        userTxt.setForeground(new Color(173, 173, 174));
-        userTxt.setBounds(160, 72, 200, 20);
-        userTxt.setBackground(new Color(42, 46, 51));
-        userTxt.setBorder(new LineBorder(Color.LIGHT_GRAY,1));
-        panel.add(userTxt);
-
-        passLbl = new JLabel("Password:");
-        passLbl.setForeground(new Color(173, 173, 174));
-        passLbl.setBounds(73, 110, 70, 10);
-        panel.add(passLbl);
-
-        passTxt = new JTextField();
-        passTxt.setForeground(new Color(173, 173, 174));
-        passTxt.setBounds(160, 107, 200, 20);
-        passTxt.setBackground(new Color(42, 46, 51));
-        passTxt.setBorder(new LineBorder(Color.LIGHT_GRAY,1));
-        panel.add(passTxt);
 
 
         this.add(panel);
         this.setVisible(true);
+    }
+
+    public void setCheckBoxAndSingIn(){
+
+        rememberPass = new JCheckBox("Remember me");
+        rememberPass.setBounds(70, 255, 200, 20);
+        rememberPass.setForeground(new Color(173, 173, 174));
+        rememberPass.setOpaque(false);
+        rememberPass.setFocusable(false);
+        rememberPass.setBackground(new Color(42, 46, 51));
+        rememberPass.setIconTextGap(10);
+        rememberPass.setSelectedIcon(new ImageIcon("src/LoginFrameComponents/CheckedBox.png"));
+        rememberPass.setIcon(new ImageIcon("src/LoginFrameComponents/UncheckedBox.png"));
+        panel.add(rememberPass);
+
+        loginBtn = new JButton();
+        loginBtn.setIcon(signInIcon);
+        loginBtn.setBounds(260, 300, 170, 50);
+        panel.add(loginBtn);
+
+    }
+
+    public void setFields(){
+
+        JLabel steamLabel = new JLabel();
+        steamLabel.setIcon(steamIcon);
+        steamLabel.setBounds(65, 20, 180, 70);
+        panel.add(steamLabel);
+
+        accNameLbl = new JLabel("SIGN IN WITH ACCOUNT NAME");
+        accNameLbl.setForeground(new Color(173, 173, 174));
+        accNameLbl.setBounds(70, 115, 250, 15);
+        accNameLbl.setFont(new Font(null, Font.BOLD, 15));
+        panel.add(accNameLbl);
+
+        userTxt = new JTextField();
+        userTxt.setForeground(new Color(173, 173, 174));
+        userTxt.setBounds(70, 140, 535, 35);
+        userTxt.setBackground(new Color(42, 46, 51));
+        userTxt.setCaretColor(Color.WHITE);
+        userTxt.setBorder(
+                javax.swing.BorderFactory.createCompoundBorder(null,
+                        javax.swing.BorderFactory.createEmptyBorder(1, 5, 1, 5)));
+        panel.add(userTxt);
+
+        passLbl = new JLabel("PASSWORD");
+        passLbl.setForeground(new Color(173, 173, 174));
+        passLbl.setBounds(70, 185, 250, 15);
+        passLbl.setFont(new Font(null, Font.BOLD, 15));
+        panel.add(passLbl);
+
+        passTxt = new JTextField();
+        passTxt.setForeground(new Color(173, 173, 174));
+        passTxt.setBounds(70, 210, 535, 35);
+        passTxt.setBackground(new Color(42, 46, 51));
+        passTxt.setCaretColor(Color.WHITE);
+        passTxt.setBorder(
+                javax.swing.BorderFactory.createCompoundBorder(null,
+                        javax.swing.BorderFactory.createEmptyBorder(1, 5, 1, 5)));
+        panel.add(passTxt);
+
     }
 
 }
