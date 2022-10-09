d5aeb9cdce4a30c33574822424f643ba4c8bd12c
Gebi <andreigebaila@gmail.com>
2022-10-09 12:56:34 +0100

    Finished creating the Account class

--- src/Scripts/Account.java
+++ src/Scripts/Account.java
@@ -1,22 +1,12 @@
 package Scripts;
 
-
-import java.awt.font.GlyphJustificationInfo;
 import java.io.*;
-import java.lang.reflect.Type;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Account implements Serializable{
 
-    public String username, pass;
+    private String username, pass;
     private final int id;
-
-    List<Account> accounts = new ArrayList<>();
 
     public Account(String username, String pass){
 
@@ -24,12 +14,10 @@ public class Account implements Serializable{
         this.username = username;
         this.pass = pass;
 
-        accounts.add(this);
-
-
 
     }
 
+    /** Gets a new ID for a new account */
     private int getNextId(){
         int id = 0;
         try {
@@ -50,4 +38,107 @@ public class Account implements Serializable{
         return id;
     }
 
+    /** Stores account in the DB */
+    public static void storeAccount(Account acc){
+
+        if(checkAccountExists(acc)) {
+            System.out.println("Account \"" + acc.username + "\" already exists.");
+            return;
+        }
+
+        List<Account> list = getAccountsList();
+        list.add(acc);
+
+        try {
+
+            FileOutputStream fileOut =
+                    new FileOutputStream("accounts.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(list);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /** Removes an account from the DB */
+    public static void removeAccount(Account acc){
+        if(!checkAccountExists(acc)) {
+            System.out.println("Account \"" + acc.username + "\" can't be removed because it doesn't exist.");
+            return;
+        }
+
+        List<Account> list = getAccountsList();
+
+        int i = 0;
+        for (Account account : list) {
+            if(Objects.equals(account.username, acc.username))
+                break;
+            i++;
+        }
+
+        list.remove(i);
+
+        storeAccountsList(list);
+    }
+
+    /** Checks if an account with a given name already exists */
+    public static boolean checkAccountExists(Account acc){
+        for (Account account : getAccountsList()){
+            if(Objects.equals(account.username, acc.username))
+                return true;
+        }
+        return false;
+    }
+
+    /** Returns a list of all accounts */
+    public static List<Account> getAccountsList(){
+
+        try {
+            FileInputStream fileIn = new FileInputStream("accounts.txt");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            List<Account> list = (List<Account>) in.readObject();
+            in.close();
+            fileIn.close();
+            return list;
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Employee class not found");
+            c.printStackTrace();
+        }
+
+        return new ArrayList<>();
+    }
+
+    /** Print the list of all the stored accounts */
+    public static void printAccountsList(){
+
+        for(Account account : getAccountsList())
+            System.out.println("User: " + account.username + " | Pass: " + account.pass);
+    }
+
+    /** Stores a list of accounts to the DB */
+    public static void storeAccountsList(List<Account> list){
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("accounts.txt");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(list);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void removeAllAccounts(){
+
+        List<Account> list = getAccountsList();
+        list.clear();
+        storeAccountsList(list);
+
+    }
 }
