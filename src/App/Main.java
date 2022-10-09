2971b39c234133c4a76d7249a19f434fe26f1fac
Gebi <andreigebaila@gmail.com>
2022-10-09 13:13:35 +0100

    Debugging of Account class

--- src/App/Main.java
+++ src/App/Main.java
@@ -4,14 +4,6 @@ import Scripts.Account;
 import Swing.FriendsFrameComponents.FriendsFrame;
 import Swing.LoginFrameComponents.LoginFrame;
 import Swing.MainFrameComponents.MainFrame;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import netscape.javascript.JSObject;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
 
@@ -19,55 +11,30 @@ public class Main {
     public static FriendsFrame friendsFrame;
     public static boolean loginClosed = false, mainClosed = true, friendsClosed = true;
     public static boolean loggedIn = false;
+    public static Account loggedAccount;
 
     public static void main(String[] args) {
         Account andrei = new Account("Andrei", "admin");
         Account andrei1 = new Account("Eugen", "pass");
         Account andrei2 = new Account("Pula", "mea");
 
-        List<Account> accounts = new ArrayList<>();
-        accounts.add(andrei);
-        accounts.add(andrei1);
-        accounts.add(andrei2);
+        //Account.storeAccount(andrei);
+        //Account.storeAccount(andrei1);
+        //Account.storeAccount(andrei2);
 
-        List<Account> list = new ArrayList<>();
+        //Account.removeAccount(andrei);
+        //Account.removeAccount(andrei1);
+        //Account.removeAccount(andrei2);
 
-        try {
-            FileOutputStream fileOut =
-                    new FileOutputStream("accounts.txt");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(accounts);
-            out.close();
-            fileOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            FileInputStream fileIn = new FileInputStream("accounts.txt");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            list = (List<Account>) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-            return;
-        } catch (ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-            return;
-        }
-
-        for (Account acc : list){
-            System.out.println(acc.username + " | " + acc.pass);
-        }
-
+        //Account.printAccountsList();
+        //Account.removeAllAccounts();
 
         LoginFrame loginFrame = new LoginFrame();
         while (!loggedIn){ System.out.print(""); }
         System.out.println("logged in");
         loginFrame.dispose();
-        MainFrame mainFrame = new MainFrame();
+
+        new MainFrame();
 
 
     }
