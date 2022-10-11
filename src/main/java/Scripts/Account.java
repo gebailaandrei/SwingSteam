e11fc9bd5fa4f0176bf606f019e66c8539c0b882
Gebi <andreigebaila@gmail.com>
2022-10-11 22:20:13 +0100

    Added more functions for db querying

--- src/main/java/Scripts/Account.java
+++ src/main/java/Scripts/Account.java
@@ -1,5 +1,7 @@
 package Scripts;
 
+import lombok.Getter;
+
 import java.io.*;
 import java.util.*;
 
@@ -17,128 +19,42 @@ public class Account implements Serializable{
 
     }
 
+    public Account(int id, String username, String pass){
+
+        this.id = id;
+        this.username = username;
+        this.pass = pass;
+
+
+    }
     /** Gets a new ID for a new account */
     private int getNextId(){
-        int id = 0;
-        try {
-            File file = new File("nextid.txt");
-
-            id = Integer.parseInt(new Scanner(file).nextLine());
-
-            FileWriter writer = new FileWriter("nextid.txt");
-            writer.write(String.valueOf(++id));
-            writer.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return id;
+        return DB.getNumberOfAccounts() + 1;
     }
-
     /** Stores account in the DB */
     public static void storeAccount(Account acc){
-
-        if(checkAccountExists(acc)) {
-            System.out.println("Account \"" + acc.username + "\" already exists.");
-            return;
-        }
-
-        List<Account> list = getAccountsList();
-        list.add(acc);
-
-        try {
-
-            FileOutputStream fileOut =
-                    new FileOutputStream("accounts.txt");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(list);
-            out.close();
-            fileOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        DB.insertAccount(acc.id, acc.username, acc.pass);
     }
-
     /** Removes an account from the DB */
     public static void removeAccount(Account acc){
-        if(!checkAccountExists(acc)) {
-            System.out.println("Account \"" + acc.username + "\" can't be removed because it doesn't exist.");
-            return;
-        }
-
-        List<Account> list = getAccountsList();
-
-        int i = 0;
-        for (Account account : list) {
-            if(Objects.equals(account.username, acc.username))
-                break;
-            i++;
-        }
-
-        list.remove(i);
-
-        storeAccountsList(list);
+        DB.deleteAccount(acc.id);
     }
-
     /** Checks if an account with a given name already exists */
     public static boolean checkAccountExists(Account acc){
-        for (Account account : getAccountsList()){
-            if(Objects.equals(account.username, acc.username))
-                return true;
-        }
-        return false;
+        return DB.checkAccountExists(acc.username);
     }
-
     /** Returns a list of all accounts */
     public static List<Account> getAccountsList(){
-
-        try {
-            FileInputStream fileIn = new FileInputStream("accounts.txt");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            List<Account> list = (List<Account>) in.readObject();
-            in.close();
-            fileIn.close();
-            return list;
-        } catch (IOException i) {
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-        }
-
-        return new ArrayList<>();
+        return DB.getAccountsList();
     }
-
     /** Print the list of all the stored accounts */
     public static void printAccountsList(){
-
         for(Account account : getAccountsList())
-            System.out.println("User: " + account.username + " | Pass: " + account.pass);
+            System.out.println("ID: " + account.id + " User: " + account.username + " Pass: " + account.pass);
     }
-
     /** Stores a list of accounts to the DB */
     public static void storeAccountsList(List<Account> list){
-        try {
-            FileOutputStream fileOut =
-                    new FileOutputStream("accounts.txt");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(list);
-            out.close();
-            fileOut.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void removeAllAccounts(){
-
-        List<Account> list = getAccountsList();
-        list.clear();
-        storeAccountsList(list);
-
+        for(Account acc : list)
+            DB.insertAccount(acc.id, acc.username, acc.pass);
     }
 }
--- src/main/java/Scripts/DB.java
+++ src/main/java/Scripts/DB.java
@@ -5,19 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /** For now files will be used instead of a database */
 public class DB {
 
-    static String url = "jdbc:mysql://127.0.0.1:3306/steam";
-    static String uname = "root";
-    static String pass = "admin";
+    private static String url = "jdbc:mysql://127.0.0.1:3306/steam";
+    private static String uname = "root";
+    private static String pass = "admin";
 
-    static Connection con;
-    static Statement statement;
-    static ResultSet result;
-    static String query;
+    private static Connection con;
+    private static Statement statement;
+    private static ResultSet result;
+    private static String query;
 
+    /** USE THIS FOR MAkiNG CHANGES TO THE DB WITHOUT RUNNING THE WHOLE PROJECT */
+    public static void main(String[] args) {
+        DB db = new DB();
+
+
+        Account.printAccountsList();
+
+        //insertAccount(5, "andrei", "pass");
+        //deleteAccount("andrei");
+        //System.out.println(getNumberOfAccounts());
+
+    }
+    /** DB object needs to be created when program start running to establish the connection with the database */
     public DB() {
 
         try {
@@ -27,10 +43,44 @@ public class DB {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
-        System.out.println(getAccDetails(1));
     }
+    /** Adds an account to the database with the specified details */
+    public static void insertAccount(int id, String username, String password){
 
+        query = "INSERT INTO accounts (id, username, pass) VALUES (" + id + ", '" + username + "', '" + password + "');";
+
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    /** Deletes an account from the database corresponding to the specified id */
+    public static void deleteAccount(int id){
+
+        query = "DELETE FROM accounts WHERE id = " + id + ";";
+
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    /** Deletes an account from the database corresponding to the specified username */
+    public static void deleteAccount(String username){
+
+        query = "DELETE FROM accounts WHERE username = '" + username + "';";
+
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    /** Gets the id of the account with the specified username */
     public static int getID(String user){
 
         query = "SELECT id FROM accounts WHERE username = '" + user + "';";
@@ -43,7 +93,7 @@ public class DB {
             throw new RuntimeException(e);
         }
     }
-
+    /** Gets the username of the account with the specified id */
     public static String getUsername(int id){
 
         query = "SELECT username FROM accounts WHERE id = '" + id + "';";
@@ -56,7 +106,7 @@ public class DB {
             throw new RuntimeException(e);
         }
     }
-
+    /** Gets the password of the account with the specified username */
     public static String getPassword(String user){
 
         query = "SELECT pass FROM accounts WHERE username = '" + user + "';";
@@ -69,25 +119,71 @@ public class DB {
             throw new RuntimeException(e);
         }
     }
-
+    /** Gets the id, username and password of the account with the specified id */
     public static String[] getAccDetails(int id){
 
         query = "SELECT * FROM accounts WHERE id = '" + id + "';";
         String[] details = new String[3];
 
         try {
-            PreparedStatement stmt = con.prepareStatement(
-                    query,
-                    ResultSet.TYPE_SCROLL_SENSITIVE,
-                    ResultSet.CONCUR_UPDATABLE);
-            result = stmt.executeQuery(query);
-
+            result = statement.executeQuery(query);
+            result.next();
             for(int i = 1; i <= 3; i++)
-                System.out.println(result.getString(i));
+                details[i-1] = result.getString(i);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return details;
     }
+    /** Returns the number of accounts in the db */
+    public static int getNumberOfAccounts(){
+        query = "SELECT COUNT(*) FROM accounts;";
 
+        try {
+            result = statement.executeQuery(query);
+            result.next();
+            return result.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /** Wipe accounts table */
+    private void wipeAccounts(){
+        query = "DELETE * FROM accounts";
+
+        try {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean checkAccountExists(String username){
+        query = "SELECT * FROM accounts WHERE username = '" + username + "';";
+
+        try {
+            result = statement.executeQuery(query);
+            return result.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Account> getAccountsList(){
+
+        query = "SELECT * FROM accounts;";
+        List<Account> list = new ArrayList<>();
+
+        try {
+            result = statement.executeQuery(query);
+            while (result.next())
+                list.add(new Account(result.getInt(1),
+                        result.getString("username"),
+                        result.getString("pass")));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return list;
+    }
 }
