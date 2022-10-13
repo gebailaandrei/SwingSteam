66754779de0bb1c779431b46f70daabf61e65ed5
Gebi <andreigebaila@gmail.com>
2022-10-13 21:52:40 +0100

    Refactoring

---
+++ src/main/java/Swing/MainFrameComponents/MainFrameMiddlePanel/DefaultMiddlePanel.java
@@ -0,0 +1,16 @@
+package Swing.MainFrameComponents.MainFrameMiddlePanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DefaultMiddlePanel extends JPanel {
+
+    public static JFrame parentFrame;
+
+    public DefaultMiddlePanel(){
+
+        this.setBackground(new Color(39, 43, 47));
+
+    }
+
+}
