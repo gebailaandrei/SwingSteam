917f78a48a5401bb635e23a64f2e3232248ee813
Gebi <andreigebaila@gmail.com>
2022-10-16 19:50:23 +0100

    Class for game objects to help use them in app

---
+++ src/main/java/Scripts/Game.java
@@ -0,0 +1,15 @@
+package Scripts;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+//* This class is used for getting games from the db as Game objects, so they can be listed in the app */
+@Getter
+@AllArgsConstructor
+public class Game {
+
+    private int GID;
+    private String gameName, picURL, description;
+
+
+}
