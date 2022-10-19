0d4cebc3108f28fd2f247bf910d394f0dcc4eff9
Gebi <andreigebaila@gmail.com>
2022-10-19 21:53:00 +0100

    More methods that allow loading images at specified path

--- src/main/java/Scripts/ImageHandler.java
+++ src/main/java/Scripts/ImageHandler.java
@@ -16,13 +16,20 @@ public class ImageHandler {
     public static final String STEAM_ICON = "SteamIcon.png";
     public static final String UNCHECKED_BOX = "UncheckedBox.png";
     public static final String STEAM_FRAME_BAR_ICON = "SteamFrameBarIcon.png";
+    public static final String THUMBS_UP_RATING = "ThumbsUpRating.png";
 
     /** Loads an image from the project files and returns it as a ImageIcon */
     public static ImageIcon loadImageIcon(String imgName){
+        return new ImageIcon(ImageHandler.path + imgName);
+    }
+    public static ImageIcon loadImageIcon(String path, String imgName){
         return new ImageIcon(path + imgName);
     }
     public static Image loadImage(String imgName){
         return loadImageIcon(imgName).getImage();
+    }
+    public static Image loadImage(String path, String imgName){
+        return loadImageIcon(path, imgName).getImage();
     }
     /** Resizes image and returns an ImageIcon object */
     public static ImageIcon resizeImageIcon(int newWidth, int newHeight, Image img){
