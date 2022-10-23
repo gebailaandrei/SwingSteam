ea3a2169357bb3b11dcb713cf726f76c60dd2ed9
Gebi <andreigebaila@gmail.com>
2022-10-23 21:50:35 +0100

    Removed interface

--- src/main/java/Swing/Frames/MainFrameComponents/MainFrameMiddlePanels/MainFrameMiddlePanel.java
+++
@@ -1,9 +0,0 @@
-package Swing.Frames.MainFrameComponents.MainFrameMiddlePanels;
-
-import javax.swing.*;
-
-public interface MainFrameMiddlePanel{
-
-
-    void updatePanelSize(int width, int height);
-}
